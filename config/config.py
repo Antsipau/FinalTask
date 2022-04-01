33072084a1fa2cdb510f62f57cf3baba46768241
Antsipau <antsipau@tut.by>
2022-04-01 17:23:17 +0300

    new files

--- config/config.py
+++ config/config.py
@@ -2,6 +2,7 @@ class TestData:
     CHROME_EXECUTABLE_PATH = "/home/jrankel/resources/chromedriver"
 
     BASE_URL = "http://localhost:8000/"
+    # BASE_URL = "http://localhost:8000/admin/login/?next=/admin/"
     USER_NAME = "admin"
     PASSWORD = "password"
 
